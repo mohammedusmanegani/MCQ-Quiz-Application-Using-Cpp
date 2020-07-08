4cae725507439ad1c3ef5cb0be4008547c457552
Mohammed Usman E Gani <ka.usmanegani@gmail.com>
2020-07-08 08:15:34 +0530

    Initial Commit

---
+++ MCQ Pro/MCQ Pro.cpp
@@ -0,0 +1,537 @@
+#include<iostream.h>
+#include<iomanip.h>
+#include<conio.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+void main()
+{
+	int n,p,z1,z2,z3,z4,z5,z6,z7,z8,z9,z10,s,q,b,d;
+
+    do{
+		
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+    cout<<"                        MCQ  (MULTIPLE CHOICE QUESTIONS)"<<endl;
+	cout<<" "<<endl;
+    cout<<"                       User Name : Mohammed Usman E Gani"<<endl;
+    cout<<"                       Password  : ";
+	cin>>p;
+ 
+	
+
+ 
+	system("CLS");
+     
+	
+	
+	 
+	
+	}
+	while(p!=583102);
+
+	system("ClS");
+	
+	
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+    cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<"                                 * WEL COME *"<<endl;
+		cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<"                                               Press ENTER to  Continue ..."<<endl;
+	getch();
+	system("ClS");
+		
+	
+	
+	cout<<" "<<endl;
+	cout<<"                          *** Multiple choice questions ***"<<endl;
+	cout<<" "<<endl;
+	cout<<" TOPIC : C++ basics"<<endl;
+    cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<" 1.The Programming language among the following is:"<<endl;
+    cout<<" "<<endl;
+	cout<<" 1. JAVA"<<endl;
+	cout<<" 2. Kannada"<<endl;
+	cout<<" 3. Both 1 & 4"<<endl;
+	cout<<" 4. C++"<<endl;
+	cout<<" "<<endl;
+	cout<<" Enter the correct option:";
+	cout<<" ";
+	cin>>n;
+	while(n>4){
+		cout<<" "<<endl;
+	    cout<<" "<<endl;
+		cout<<" INVALID OPTION "<<endl;
+		cout<<" "<<endl;
+		cout<<" VALID OPTIONS ARE (1) (2) (3) (4) "<<endl;
+		cout<<" "<<endl;
+		cout<<" Please re-enter valid option ";
+		cin>>n;
+    
+	}
+	if(n==3)
+        
+		z1=1;
+	
+	else
+		z1=0;
+	    
+	    cout<<"   "<<endl;
+		
+	system("ClS");
+	
+ cout<<" "<<endl;
+	cout<<" "<<endl;
+	
+	cout<<" 2.Which of the following is web Designing programming language:"<<endl;
+    cout<<" "<<endl;
+	cout<<" 1. HTML"<<endl;
+	cout<<" 2. C++"<<endl;
+	cout<<" 3. C--"<<endl;
+	cout<<" 4. SQL"<<endl;
+	cout<<" "<<endl;
+	cout<<" Enter the correct option:";
+	cout<<" ";
+	cin>>n;
+	while(n>4){
+		cout<<" "<<endl;
+	    cout<<" "<<endl;
+		cout<<" INVALID OPTION "<<endl;
+		cout<<" "<<endl;
+		cout<<" VALID OPTIONS ARE (1) (2) (3) (4) "<<endl;
+		cout<<" "<<endl;
+		cout<<" Please re-enter valid option ";
+		cin>>n;
+    
+	}
+	if(n==1)
+
+		z2=1;
+	else
+		z2=0;
+	    cout<<"   "<<endl;
+	
+
+	system("ClS");
+		 cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<" 3.What is #include<iostream.h>:"<<endl;
+    cout<<" "<<endl;
+	cout<<" 1. Reader file"<<endl;
+	cout<<" 2. Source file"<<endl;
+	cout<<" 3. Header file"<<endl;
+	cout<<" 4. None"<<endl;
+	cout<<" "<<endl;
+	cout<<" Enter the correct option:";
+	cout<<" ";
+	cin>>n;
+	while(n>4){
+		cout<<" "<<endl;
+	    cout<<" "<<endl;
+		cout<<" INVALID OPTION "<<endl;
+		cout<<" "<<endl;
+		cout<<" VALID OPTIONS ARE (1) (2) (3) (4) "<<endl;
+		cout<<" "<<endl;
+		cout<<" Please re-enter valid option ";
+		cin>>n;
+    
+	}
+	if(n==3)
+
+		z3=1;
+	else
+		z3=0;
+	    cout<<"   "<<endl;
+	
+
+
+	system("ClS");
+	 cout<<" "<<endl;
+	cout<<" "<<endl;
+			
+	cout<<" 4.Why is *clrscr* used in program:"<<endl;
+    cout<<" "<<endl;
+	cout<<" 1. To Display"<<endl;
+	cout<<" 2. To clearing a screen"<<endl;
+	cout<<" 3. To take input"<<endl;
+	cout<<" 4. To make program correct"<<endl;
+	cout<<" "<<endl;
+	cout<<" Enter the correct option:";
+	cout<<" ";
+	cin>>n;
+	while(n>4){
+		cout<<" "<<endl;
+	    cout<<" "<<endl;
+		cout<<" INVALID OPTION "<<endl;
+		cout<<" "<<endl;
+		cout<<" VALID OPTIONS ARE (1) (2) (3) (4) "<<endl;
+		cout<<" "<<endl;
+		cout<<" Please re-enter valid option ";
+		cin>>n;
+    
+	}
+	if(n==2)
+
+		z4=1;
+	else
+		z4=0;
+	    cout<<"   "<<endl;
+	
+
+	system("ClS");
+		 cout<<" "<<endl;
+	cout<<" "<<endl;		
+	cout<<" 5.*getch()* comes under which header file:"<<endl;
+    cout<<" "<<endl;
+	cout<<" 1. #include<iostream.h>"<<endl;
+	cout<<" 2. #include<conio.h>"<<endl;
+	cout<<" 3. #include<iomanip.h>"<<endl;
+	cout<<" 4. #include<fstream.h>"<<endl;
+	cout<<" "<<endl;
+	cout<<" Enter the correct option:";
+	cout<<" ";
+	cin>>n;
+	while(n>4){
+		cout<<" "<<endl;
+	    cout<<" "<<endl;
+		cout<<" INVALID OPTION "<<endl;
+		cout<<" "<<endl;
+		cout<<" VALID OPTIONS ARE (1) (2) (3) (4) "<<endl;
+		cout<<" "<<endl;
+		cout<<" Please re-enter valid option ";
+		cin>>n;
+    
+	}
+	if(n==2)
+
+		z5=1;
+	else
+		z5=0;
+	    cout<<"   "<<endl;
+	
+
+
+
+	system("ClS");
+	 cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<" 6.class must be ended with:"<<endl;
+    cout<<" "<<endl;
+	cout<<" 1. :"<<endl;
+	cout<<" 2. ;"<<endl;
+	cout<<" 3. +"<<endl;
+	cout<<" 4. ="<<endl;
+	cout<<" "<<endl;
+	cout<<" Enter the correct option:";
+	cout<<" ";
+	cin>>n;
+while(n>4){
+		cout<<" "<<endl;
+	    cout<<" "<<endl;
+		cout<<" INVALID OPTION "<<endl;
+		cout<<" "<<endl;
+		cout<<" VALID OPTIONS ARE (1) (2) (3) (4) "<<endl;
+		cout<<" "<<endl;
+		cout<<" Please re-enter valid option ";
+		cin>>n;
+    
+	}
+	if(n==2)
+
+		z6=1;
+	else
+		z6=0;
+	    cout<<"   "<<endl;
+	
+
+	system("ClS");
+				 cout<<" "<<endl;
+	cout<<" "<<endl;		
+	cout<<" 7.int holds which type of data :"<<endl;
+    cout<<" "<<endl;
+	cout<<" 1. Integer"<<endl;
+	cout<<" 2. Float"<<endl;
+	cout<<" 3. Charachter"<<endl;
+	cout<<" 4. NIL"<<endl;
+	cout<<" "<<endl;
+	cout<<" Enter the correct option:";
+	cout<<" ";
+	cin>>n;
+	while(n>4){
+		cout<<" "<<endl;
+	    cout<<" "<<endl;
+		cout<<" INVALID OPTION "<<endl;
+		cout<<" "<<endl;
+		cout<<" VALID OPTIONS ARE (1) (2) (3) (4) "<<endl;
+		cout<<" "<<endl;
+		cout<<" Please re-enter valid option ";
+		cin>>n;
+    
+	}
+	if(n==1)
+
+		z7=1;
+	else
+		z7=0;
+	    cout<<"   "<<endl;
+	
+
+
+	system("ClS");
+		 cout<<" "<<endl;
+	cout<<" "<<endl;					
+	cout<<" 8.<iomanip.h>..............:"<<endl;
+    cout<<" "<<endl;
+	cout<<" 1. Input & output"<<endl;
+	cout<<" 2. Input-output motors"<<endl;
+	cout<<" 3. Input-output manipulators"<<endl;
+	cout<<" 4. Graphics"<<endl;
+	cout<<" "<<endl;
+	cout<<" Enter the correct option:";
+	cout<<" ";
+	cin>>n;
+	
+while(n>4){
+		cout<<" "<<endl;
+	    cout<<" "<<endl;
+		cout<<" INVALID OPTION "<<endl;
+		cout<<" "<<endl;
+		cout<<" VALID OPTIONS ARE (1) (2) (3) (4) "<<endl;
+		cout<<" "<<endl;
+		cout<<" Please re-enter valid option ";
+		cin>>n;
+    
+	}
+
+	if(n==3)
+
+	z8=1;
+	else
+		z8=0;
+	    cout<<"   "<<endl;
+	
+
+
+	system("ClS");
+	 cout<<" "<<endl;
+	cout<<" "<<endl;							
+	cout<<" 9.i++ means ...........:"<<endl;
+    cout<<" "<<endl;
+	cout<<" 1. incrementation in a"<<endl;
+	cout<<" 2. incrementation in b"<<endl;
+	cout<<" 3. incrementation in c"<<endl;
+	cout<<" 4. incrementation in i"<<endl;
+	cout<<" "<<endl;
+	cout<<" Enter the correct option:";
+	cout<<" ";
+	cin>>n;
+while(n>4){
+		cout<<" "<<endl;
+	    cout<<" "<<endl;
+		cout<<" INVALID OPTION "<<endl;
+		cout<<" "<<endl;
+		cout<<" VALID OPTIONS ARE (1) (2) (3) (4) "<<endl;
+		cout<<" "<<endl;
+		cout<<" Please re-enter valid option ";
+		cin>>n;
+    
+	}
+	if(n==4)
+
+		z9=1;
+	else
+		z9=0;
+	    cout<<"   "<<endl;
+	
+
+
+				
+	system("ClS");
+	 cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<" 10.Campiler is ...........:"<<endl;
+    cout<<" "<<endl;
+	cout<<" 1. Hard ware"<<endl;
+	cout<<" 2. system Soft ware"<<endl;
+	cout<<" 3. Tupper ware"<<endl;
+	cout<<" 4. OS"<<endl;
+	cout<<" "<<endl;
+	cout<<" Enter the correct option:";
+	cout<<" ";
+	cin>>n;
+	while(n>4){
+		cout<<" "<<endl;
+	    cout<<" "<<endl;
+		cout<<" INVALID OPTION "<<endl;
+		cout<<" "<<endl;
+		cout<<" VALID OPTIONS ARE (1) (2) (3) (4) "<<endl;
+		cout<<" "<<endl;
+		cout<<" Please re-enter valid option ";
+		cin>>n;
+    
+	}
+	if(n==2)
+
+		z10=1;
+	else
+		z10=0;
+	    cout<<"   "<<endl;
+	
+
+	system("ClS");
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+		cout<<"********************************************************************************"<<endl;
+		cout<<"                             ! ! ! THANKYOU ! ! !"<<endl;
+		cout<<" "<<endl;
+		cout<<"                       DEVELOPED BY MOHAMMED USMAN -E- GANI"<<endl;
+		cout<<"********************************************************************************"<<endl;
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<" Press 1 to show result"<<endl;
+	
+	cout<<" "<<endl;
+	cout<<" Press 2 to EXIT"<<endl;
+	
+	cout<<" "<<endl;
+
+
+	cout<<" Enter your choice: ";
+	cin>>q;
+	
+	system("CLS");
+	cout<<" "<<endl;
+	s=z1+z2+z3+z4+z5+z6+z7+z8+z9+z10;
+	b=10-s;
+	if(q==1)
+	{
+		cout<<"                           ------------------------------ "<<endl;
+		cout<<"                              ***  RESULT  ***"<<endl;
+		
+
+    	cout<<"                           ------------------------------ "<<endl;
+     	cout<<"  "<<endl;
+        cout<<"                           Total correct responce = "<<s<<endl;
+		cout<<" "<<endl;
+        cout<<"                           Total wrong responce   = "<<b<<endl;
+
+        cout<<" "<<endl;
+		cout<<" "<<endl;
+        
+        cout<<" "<<endl;
+	    cout<<"                           ------------------------------ "<<endl;
+		if(s==5)
+			cout<<"                            Result = AVERAGE PEEFORMANCE ";
+		if(s<5)
+			cout<<"                            Result = POOR PEEFORMANCE";
+		if(s==6||s==7)
+            cout<<"                            Result = BETTER PEEFORMANCE";
+		if(s>7)
+			cout<<"                            Result = EXECELENT PEEFORMANCE";
+    
+	cout<<" "<<endl;
+    	cout<<"                           ------------------------------ "<<endl;
+	cout<<"                                                     Press Enter to coninue..."<<endl;
+
+	getch();
+    system("CLS");
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<" To Display the wrong responced questions Press 1 :"<<endl;
+	cout<<" "<<endl;
+	cout<<" To Exit Press 2 :"<<endl;
+	cout<<" "<<endl;
+	cout<<" Enter your choice : ";
+	cin>>d;
+    system("CLS");
+		if(d==1)
+		{
+			cout<<" "<<endl;
+	cout<<" The following are the wrong responced questions. "<<endl;
+	   cout<<" "<<endl;
+	cout<<" "<<endl;
+			if(z1==0)
+			{
+				cout<<" "<<endl;
+	cout<<" 1.The Programming language among the following is:"<<endl;
+    cout<<" "<<endl;
+	cout<<" 1. (JAVA)"<<endl;
+	cout<<" 2. Kannada"<<endl;
+	cout<<" 3. Both 1 & 4"<<endl;
+	cout<<" 4. C++"<<endl;
+	cout<<" "<<endl;
+    cout<<" "<<endl;
+	cout<<" !! Oops !! The correct answer was option 1."<<endl;
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+			}
+			if(z2==0)
+			{
+	cout<<" "<<endl;
+	
+	cout<<" 2.Which of the following is web Designing programming language:"<<endl;
+    cout<<" "<<endl;
+	cout<<" 1. HTML"<<endl;
+	cout<<" 2. C++"<<endl;
+	cout<<" 3. C--"<<endl;
+	cout<<" 4. SQL"<<endl;
+	cout<<" "<<endl;
+	 cout<<" "<<endl;
+	cout<<" !! Oops !! The correct answer was option 1."<<endl;
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+			}
+		}
+		else
+		{
+           exit(0);
+		}
+	cout<<" "<<endl;
+		cout<<" "<<endl;
+	cout<<" "<<endl;
+    cout<<" "<<endl;
+ 
+	cout<<"                                           !!! Thankyou Press enter to exit..."<<endl; 
+	}
+	if(q==2)
+	{
+		exit(0);
+	}
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+		
+	getch();
+}
+
+
